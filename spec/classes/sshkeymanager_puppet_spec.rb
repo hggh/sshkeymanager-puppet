06ba76e91436c2817de4526c30357eb8a2bdd78a
Jonas Genannt <jonas@brachium-system.net>
2015-04-11 15:12:25 +0200

    added rspec tests

--- spec/classes/sshkeymanager_puppet_spec.rb
+++ spec/classes/sshkeymanager_puppet_spec.rb
@@ -1,34 +1,30 @@
 require 'spec_helper'
 
-describe 'sshkeymanager' do
+describe 'sshkeymanager::puppet' do
 
   context 'with defaults for all parameters' do
-    it { should contain_class('sshkeymanager') }
+    it { should contain_class('sshkeymanager::puppet') }
 
-    it { should contain_class('sshkeymanager::params') }
-
-    it { should contain_file('/etc/sshkeymanager').with(
+    it { should contain_file('/etc/sshkeymanager-hiera').with(
        'ensure' => 'directory',
        'owner'  => 'root',
        'group'  => 'root',
        'mode'   => '0755',
-       'purge'  => 'true',
     )}
   end
 
   context 'with different parameters' do
     let (:params) {
        {
-       :directory => '/etc/ssh/keys'
+       :directory => '/etc/puppet/sshkeymanager'
        }
     }
-    it { should contain_class('sshkeymanager') }
-    it { should contain_file('/etc/ssh/keys').with(
+    it { should contain_class('sshkeymanager::puppet') }
+    it { should contain_file('/etc/puppet/sshkeymanager').with(
        'ensure' => 'directory',
        'owner'  => 'root',
        'group'  => 'root',
        'mode'   => '0755',
-       'purge'  => 'true',
     )}
   end
 end
